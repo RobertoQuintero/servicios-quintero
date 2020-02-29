b5a6d58442bb8696230666979cf17d156561925c
Roberto Carlos Quintero <langdonhp435@gmail.com>
2020-02-29 16:32:47 -0600

    Se agregan librerias wowjs y animatecss

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,7 @@ import toggle from './components/toggle';
 import slider from './components/slider';
 import scroll from './components/scroll';
 import lightbox from './components/lightbox'
+import WOW from './components/wow'
+
+new WOW().init()
 
